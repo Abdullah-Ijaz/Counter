b245ebb5d11cec1d5fcc7228a715ff9bcc8834a0
Abdullah Ijaz <59234917+Abdullah-Ijaz@users.noreply.github.com>
2025-11-23 07:48:13 -0500

    Refactor comment in Script.js
    Updated comment for clarity and removed outdated link.

--- Script.js
+++ Script.js
@@ -37,5 +37,5 @@ function updateCounterDisplay() {
     actionBtn.disabled = count === 0;
 }
 
-    //(check this:  https://www.google.com/search?q=abling+a+disabled+button+using+javascript&rlz=1C1CHBF_enUS895US895&oq=abling+a+disabled+button+using+javascript&aqs=chrome..69i57j0l6.32383j1j4&sourceid=chrome&ie=UTF-8)
+    //(Note to self:: https://www.google.com/search?q=abling+a+disabled+button+using+javascript&rlz=1C1CHBF_enUS895US895&oq=abling+a+disabled+button+using+javascript&aqs=chrome..69i57j0l6.32383j1j4&sourceid=chrome&ie=UTF-8)
     //here we remove the d none tag which was keeping HTML from showing the text of review. Reference Index.HTML for that. we have above just refernced the button named See-review. It was then given an event listener which has a class which contained the d-none. We made conditionals that if the d-none is active, or if the event we are listening for our click, JS is checking if it contains d-none and then removing it with the remove () and then writing or giving the button the desired text-content. Now the d-none class is no longer active which is our else block and since button is still in scope, we use it to change the text-content again and back to See Review.
